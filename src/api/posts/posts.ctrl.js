b422cd402d12c707cf2a8893fc9addcab62a23cd
Theo Cho <cdsleaf@gmail.com>
2022-01-09 17:00:39 +1300

    apply JWT

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -4,10 +4,31 @@ import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
   if (!ObjectId.isValid(id)) {
     ctx.status = 400;
+    return;
+  }
+  try {
+    const post = await Post.findById(id);
+
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const checkOwnPost = (ctx, next) => {
+  const { user, post } = ctx.state;
+
+  if (post.user._id.toString() !== user._id) {
+    ctx.status = 403;
     return;
   }
   return next();
@@ -32,7 +53,7 @@ export const write = async ctx => {
   }
 
   const { title, body, tags } = ctx.request.body;
-  const post = new Post({ title, body, tags });
+  const post = new Post({ title, body, tags, user: ctx.state.user });
   
   try {
     await post.save();
@@ -51,14 +72,20 @@ export const list = async ctx => {
     return;
   }
 
+  const { tag, username } = ctx.query;
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+
   try {
-    const posts = await Post.find()
+    const posts = await Post.find(query)
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
       .lean()
       .exec();
-    const postCount = await Post.countDocuments().exec();
+    const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
     ctx.body = posts.map(post => ({
       ...post,
@@ -71,17 +98,7 @@ export const list = async ctx => {
 
 // GET /api/posts/:id
 export const read = async ctx => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if (!post) {
-      ctx.status = 404;
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
+  ctx.body = ctx.state.post;
 };
 
 // DELETE /api/posts/:id
---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+UserSchema.methods.setPassword = async function(password) {
+  this.hashedPassword = await bcrypt.hash(password, 10);
+}
+UserSchema.methods.checkPassword = async function(password) {
+  return await bcrypt.compare(password, this.hashedPassword);
+}
+UserSchema.methods.serialize = function() {
+  const data = this.toJSON();
+  delete data.hashedPassword;
+  return data;
+}
+UserSchema.methods.generateToken = function() {
+  const token = jwt.sign(
+    {
+      _id: this.id,
+      username: this.username,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '7d',
+    },
+  );
+  return token;
+}
+UserSchema.statics.findByUsername = function(username) {
+  return this.findOne({ username });
+}
+
+const User = mongoose.model('User', UserSchema);
+export default User;
